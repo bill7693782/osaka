2e612aa3291f607a74598edb0c06536cfd86971b
bill7693782 <biyang@nvidia.com>
2026-09-03 16:24:16 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 關西五日 · Service Worker v4 */
-var CACHE='kansai5-v37';
+var CACHE='kansai5-v62';
 var CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 
 self.addEventListener('install',function(e){
